c2c825e7e5833ad7e0afec8d42f1deb559e6fc8e
Дмитрий Мехонцев <mekhontsev@gmail.com>
2026-01-06 12:27:27 +0700

    WebGL2.0 for WASM

--- backends/imgui_impl_opengl3.h
+++ backends/imgui_impl_opengl3.h
@@ -55,9 +55,9 @@ IMGUI_IMPL_API void     ImGui_ImplOpenGL3_UpdateTexture(ImTextureData* tex);
 #if defined(__APPLE__)
 #include <TargetConditionals.h>
 #endif
-#if (defined(__APPLE__) && (TARGET_OS_IOS || TARGET_OS_TV)) || (defined(__ANDROID__))
+#if defined(__EMSCRIPTEN__) || (defined(__APPLE__) && (TARGET_OS_IOS || TARGET_OS_TV)) || (defined(__ANDROID__))
 #define IMGUI_IMPL_OPENGL_ES3               // iOS, Android  -> GL ES 3, "#version 300 es"
-#elif defined(__EMSCRIPTEN__) || defined(__amigaos4__)
+#elif defined(__amigaos4__)
 #define IMGUI_IMPL_OPENGL_ES2               // Emscripten    -> GL ES 2, "#version 100"
 #else
 // Otherwise imgui_impl_opengl3_loader.h will be used.
